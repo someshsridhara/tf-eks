bdb6031a793253935c07e4bea7daad457e72d776
Sridhara, Somesha (AU - Sydney) <ssridhara@deloitte.com.au>
2021-01-18 15:18:21 +1100

    test: source profile to get go

--- .buildkite/scripts/terraform-test.sh
+++ .buildkite/scripts/terraform-test.sh
@@ -4,5 +4,8 @@
  echo "Switching to test dir [$GO_TEST_DIR]..."
  cd $GO_TEST_DIR
 
+ echo "Source profile to ensure GO is available..."
+ source ~/.bash_profile
+
  echo "Running unit tests..."
  go test -v -timeout $GO_TEST_TIMEOUT
