7652d56a3988cc5447000676228a21e18833500f
Sridhara, Somesha (AU - Sydney) <ssridhara@deloitte.com.au>
2021-01-18 15:24:13 +1100

    test: remove profile sourcing

--- .buildkite/scripts/terraform-test.sh
+++ .buildkite/scripts/terraform-test.sh
@@ -4,8 +4,5 @@
  echo "Switching to test dir [$GO_TEST_DIR]..."
  cd $GO_TEST_DIR
 
- echo "Source profile to ensure GO is available..."
- source ~/.bash_profile
-
  echo "Running unit tests..."
  go test -v -timeout $GO_TEST_TIMEOUT
